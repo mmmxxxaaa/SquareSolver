b182dd1f242b3e2e1ca5c1432ed66a7ae0834f5a
mmmxxxaaa <maks.katkov.2007@mail.ru>
2025-08-13 22:34:05 +0300

    Улучшена удобочитаемость кода и исправлена опечатка
    1. in the main() float solution_case -> int solution_case
    2. added enum possible_solution_cases to use it in general_solution and general_output
    3. changed the order of function definitions and prototypes (now the order of definitions is the same as that of declarations)
    4. added continue_request()

--- main.c
+++ main.c
@@ -4,12 +4,14 @@
 #include <math.h>
 
 /*Changes
-1. added general_solution
-2. added general_output -> solved "root(s)" problem
+1. in the main() float solution_case -> int solution_case
+2. added enum possible_solution cases to use it in general_solution and general_output
+3. changed the order of function definitions and prototypes (now the order of definitions is the same as that of declarations)
+4. added continue_request()
 */
 
-int is_zero(float number);
-void clear_input_stream(void);
+enum possible_solution_cases {all_abc_coef_0, only_ab_coef_0, only_a_coef_0,
+                              discriminant_equal_to_0, discriminant_more_than_0, discriminant_less_than_0};
 
 void get_coeffs(float *ptr_a, float *ptr_b, float *ptr_c);
 float make_discriminant(float coeff_a, float coeff_b, float coeff_c);
@@ -19,6 +21,10 @@ int solve_normal_quadratic_equation(float * ptr_x1, float * ptr_x2, float coeff_
 
 int general_solution(float * ptr_x1, float * ptr_x2, int * ptr_amount_of_roots, float coeff_a, float coeff_b, float coeff_c);  // mb add right_part later
 void general_output(float x1, float x2, int amount_of_roots, int solution_case);
+
+int is_zero(float number);
+void clear_input_stream(void);
+int continue_request();
 
 int main()
 {
@@ -38,46 +44,21 @@ int main()
         */
         clear_input_stream();
 
-        float solution_case;
+        int solution_case;
         float x1, x2;
         int amount_of_roots;
 
         solution_case = general_solution(&x1, &x2, &amount_of_roots, coef_a, coef_b, coef_c);
         general_output(x1, x2, amount_of_roots, solution_case);
-
-        puts("Enter new quadratic quadratic equation coefficients in the following format: \"a b c\", where ax^2 +- bx +- c = 0");
-        puts("Or enter 'q' to quit the program");
-        stop_ch = getchar();
-        if (stop_ch != 'q')
-            ungetc(stop_ch, stdin);
-        else
+        if (continue_request() == -1)
             break;
     }
 }
-
-int is_zero(float number)
-{
-    const float epsilon = 1e-7;
-    return fabs(number) < epsilon;
-}
-
 
 void get_coeffs(float * ptr_a, float * ptr_b, float * ptr_c)
 {
     scanf("%f %f %f", ptr_a, ptr_b, ptr_c);
 
-}
-
-void search_right_part(float * ptr_right_part)    //Not necessary function yet
-{
-    char ch;
-
-    while ((ch = getchar()) != '\n' && ch != EOF)
-        if (ch == '=')
-        {
-            scanf("%f", ptr_right_part);
-            break;
-        }
 }
 
 float make_discriminant(float coef_a, float coef_b, float coef_c)
@@ -91,6 +72,18 @@ void make_roots(float *ptr_x1, float *ptr_x2, float coef_a, float coef_b, float 
 
     *ptr_x1 = ((-1) * coef_b + discr_square_root) / (2 * coef_a);
     *ptr_x2 = ((-1) * coef_b - discr_square_root) / (2 * coef_a);
+}
+
+void search_right_part(float * ptr_right_part)    //Not necessary function yet
+{
+    char ch;
+
+    while ((ch = getchar()) != '\n' && ch != EOF)
+        if (ch == '=')
+        {
+            scanf("%f", ptr_right_part);
+            break;
+        }
 }
 
 int solve_normal_quadratic_equation(float * ptr_x1, float * ptr_x2, float coef_a, float coef_b, float coef_c)
@@ -114,21 +107,21 @@ int solve_normal_quadratic_equation(float * ptr_x1, float * ptr_x2, float coef_a
 int general_solution(float * ptr_x1, float * ptr_x2, int * ptr_amount_of_roots, float coef_a, float coef_b, float coef_c)
 {
     if (is_zero(coef_a) && is_zero(coef_b) && is_zero(coef_c))
-        return 0;
+        return all_abc_coef_0;
     else if (is_zero(coef_a) && is_zero(coef_b))
-        return 1;
+        return only_ab_coef_0;
     else if (is_zero(coef_a))
     {
         *ptr_x1 = *ptr_x2 = -coef_c/coef_b;
-        return 2;
+        return only_a_coef_0;
     }
     else
     {
         *ptr_amount_of_roots = solve_normal_quadratic_equation(ptr_x1, ptr_x2, coef_a, coef_b, coef_c);
         if (*ptr_amount_of_roots == 1)    //discriminant = 0
-            return 3;
+            return discriminant_equal_to_0;
         else if (*ptr_amount_of_roots == 2)  //discriminant > 0
-            return 4;
+            return discriminant_more_than_0;
         else
             return 5;   //discriminant < 0
     }
@@ -138,31 +131,31 @@ void general_output(float x1, float x2, int amount_of_roots, int solution_case)
 {
     switch(solution_case)   //advisedly added everywhere one more \n to make output more readable
     {
-        case 0:
+        case all_abc_coef_0:
             puts("Any x from [-INF; +INF] is the root of this equation\n");
             break;
-        case 1:
+        case only_ab_coef_0:
             puts("This equation doesn't have any root\n");
             break;
-        case 2:
+        case only_a_coef_0:
         {
             puts("This is not an quadratic equation, but it is linear equation and it has one root:");
             printf("x = %f\n\n", x1);
             break;
         }
-        case 3:
+        case discriminant_equal_to_0:
         {
             printf("This quadratic equation has %d root\n", amount_of_roots);
             printf("x = %f\n\n", x1);
             break;
         }
-        case 4:
+        case discriminant_more_than_0:
         {
             printf("This quadratic equation has %d roots\n", amount_of_roots);
             printf("x1 = %f     x2 = %f\n\n", x1, x2);
             break;
         }
-        case 5:
+        case discriminant_less_than_0:
         {
             puts("This quadratic equation does not have roots in real numbers\n");
             break;
@@ -172,10 +165,29 @@ void general_output(float x1, float x2, int amount_of_roots, int solution_case)
     }
 }
 
+int is_zero(float number)
+{
+    const float epsilon = 1e-7;
+    return fabs(number) < epsilon;
+}
+
 void clear_input_stream()
 {
     char ch;
 
     while ((ch = getchar()) != '\n' && ch != EOF)
         continue;
+}
+
+int continue_request()
+{
+    char stop_ch;
+
+    puts("Enter new quadratic quadratic equation coefficients in the following format: \"a b c\", where ax^2 +- bx +- c = 0");
+    puts("Or enter 'q' to quit the program");
+    stop_ch = getchar();
+    if (stop_ch != 'q')
+        ungetc(stop_ch, stdin);
+    else
+        return -1;
 }
