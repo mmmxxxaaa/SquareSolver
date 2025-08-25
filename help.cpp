8edbad00c33d717ad6c1099545d1d9ce22fee1c6
mmmxxxaaa <maks.katkov.2007@mail.ru>
2025-08-25 23:38:46 +0300

    Улучшил вывод help()

--- help.cpp
+++ help.cpp
@@ -7,8 +7,11 @@ void help(const char* parameter)
 {
     system("ncal");
 
-    printf ("parameter %s\n", parameter); //ДЕЛО СДЕЛАНО вызывать ncal внутри help (календарь)
+    printf("parameter %s\n", parameter); //ДЕЛО СДЕЛАНО вызывать ncal внутри help (календарь)
+    printf("--interactive  Sets interactive mode");
     printf("Enter new quadratic quadratic equation coefficients"
         " in the following format: \"a b c\", where ax^2 + bx + c = 0 \n");
+    printf("--test         Sets the test mode");
+    printf("You should not enter anything. The information about passed tests will be shown you");
     printf ("\n");
 }
